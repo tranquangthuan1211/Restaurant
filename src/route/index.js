fe70af91f9f0d3f020d3c2eafb28dcdc529d2750
QuangThuan <tranquanthuan132@gmail.com>
2024-03-01 15:59:00 +0700

    update

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,10 @@ import Register from '../page/Register';
 import Cooking from '../binding/cartCooking'
 import DetailCooking from '../binding/DetailCooking';
 const publicRoute = [
-    { path: '/', component: Home },
-    {path:'/login',component:Login},
-    {path:"/register",component:Register},
-    {path:"/cooking",component:Cooking},
+    // { path: '/', component: Home },
+    // {path:'/login',component:Login},
+    // {path:"/register",component:Register},
+    {path:"/",component:Cooking},
     {path:"/cooking/:id",component:DetailCooking},
 ];
 const privateRoute = [];
